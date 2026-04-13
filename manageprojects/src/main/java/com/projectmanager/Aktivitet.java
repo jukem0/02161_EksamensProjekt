c2d0d44d77bb457db75ee0fadd88d7b424296ddf
Alex <alexb@live.dk>
2026-04-13 11:56:04 +0200

    A small start

---
+++ manageprojects/src/main/java/com/projectmanager/Aktivitet.java
@@ -0,0 +1,13 @@
+package com.projectmanager;
+
+public class Aktivitet {
+    private String navn;
+    private int startUge, slutUge, budgetTimer;
+
+    public Aktivitet() {
+    }
+
+    public void tilføjMedarbejder() {
+        
+    }
+}
---
+++ manageprojects/src/main/java/com/projectmanager/Medarbejder.java
@@ -0,0 +1,16 @@
+package com.projectmanager;
+
+public class Medarbejder {
+    private String initialer;
+
+    public Medarbejder() {
+    }
+
+    public void registrerTid() {
+
+    }
+
+    public void seTimer() {
+
+    }
+}
