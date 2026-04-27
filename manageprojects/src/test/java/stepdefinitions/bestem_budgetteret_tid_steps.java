035c07596260fd0d1bf1c2413a4139d4c2c27936
Gui256947 <s256947@dtu.dk>
2026-04-27 14:20:45 +0200

    opdatering og tilføjelse
    opdaterede budgetteret tid, og tilføjede tildel projektleder

--- manageprojects/src/test/java/stepdefinitions/bestem_budgetteret_tid_steps.java
+++ manageprojects/src/test/java/stepdefinitions/bestem_budgetteret_tid_steps.java
@@ -13,13 +13,20 @@ public class bestem_budgetteret_tid_steps {
     public void denne_medarbejder_er_projektleder_for_et_projekt(String medarbejder, String projekt) {
         // Implementation for checking if an employee is a project leader for a project
     }
-    @When("projektleder {medarbejder} forsøger at budgettere tid for en aktivitet {aktivitet} i projektet {projekt}")
-    public void projektleder_forsøger_at_budgettere_tid(String medarbejder, String aktivitet, String projekt) {
+    @When("medarbejderen {medarbejder} bestemmer budgetteret tid {float} for en aktivitet {aktivitet} i projektet {projekt}, som positivt decimaltal eller heltal")
+    public void medarbejder_forsøger_at_budgettere_tid(String medarbejder, float budget, String aktivitet, String projekt) {
         // Implementation for attempting to budget time for an activity in a project
     }
     @Then("budgettering af tid for aktiviteten {aktivitet} i projektet {projekt} skal lykkes")
     public void budgettering_af_tid_skal_lykkes(String aktivitet, String projekt) {
         // Implementation for verifying that budgeting time for the activity in the project is successful
     }
-    
+    @When("en medarbejder {medarbejder} bestemmer budgetteret tid {float} for en aktivitet {aktivitet} i projektet {projekt}, som andet end et positivt decimaltal eller heltal")
+    public void en_medarbejder_bestemmer_budgetteret_tid(String medarbejder, float budget, String aktivitet, String projekt) {
+        // Implementation for attempting to budget time for an activity in a project
+    }
+    @Then("budgettering af tid for aktiviteten {aktivitet} i projektet {projekt} skal fejle med fejlbesked: 'Budgetteret tid skal være et positivt decimaltal eller heltal'")
+    public void budgettering_af_tid_skal_feje(String aktivitet, String projekt) {
+        // Implementation for verifying that budgeting time for the activity in the project fails with the specified error message
+    }
 }
