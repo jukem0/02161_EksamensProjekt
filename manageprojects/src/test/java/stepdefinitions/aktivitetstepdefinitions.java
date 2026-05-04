ddf6b06513287230c2a55b8459a1b4626dfd31cc
Gui256947 <s256947@dtu.dk>
2026-05-04 15:10:35 +0200

    opdaterede opret aktivitet steps

--- manageprojects/src/test/java/stepdefinitions/aktivitetstepdefinitions.java
+++ manageprojects/src/test/java/stepdefinitions/aktivitetstepdefinitions.java
@@ -1,6 +1,9 @@
 package stepdefinitions;
 
 
+import com.projectmanager.Aktivitet;
+import com.projectmanager.Medarbejder;
+
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -8,62 +11,69 @@ import io.cucumber.java.en.When;
 
 
 public class aktivitetstepdefinitions {
+    private Aktivitet oprettetAktivitet;
     
-
     @Given("en {medarbejder} findes i systemet")
-    public Object getmedarbejder(Object medarbejder) {
-        // Implementation for setting up the employee in the system
-        Medarbejder curmedarb = new Medarbejder(medarbejder);
-        return curmedarb;   
+    public boolean getmedarbejder(String medarbejder) {
+        for (Medarbejder navn : systemMedarbejdere) {
+            if (navn.getName().equalsIgnoreCase(medarbejder)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
     }
-    @And("et projekt {String} har en projektleder {String} eller en ledig medarbejder {String}")
-    public void getprojektledighed(String projekt, String projektLeder, String ledigMedarbejder) {
-        
-        if (projektLeder.equals("Projektleder")) {
-                System.out.println("Projektleder " + projektLeder + " er tildelt til projektet " + projekt);
-            } else if (ledigMedarbejder.equals("Ledig Medarbejder")) {
-                System.out.println("Medarbejder " + ledigMedarbejder + " er tilgængelig for projektet " + projekt);
+
+    @And("et projekt {String} har en projektleder eller en ledig medarbejder {String}")
+    public boolean getProjektAnsvarlig(String projekt, String medarbejder) {
+        if (getmedarbejder(medarbejder) == true) {
+            if (erProjektleder(projekt, medarbejder) == true) {
+                System.out.println("Projektleder " + medarbejder + " er tildelt til projektet " + projekt);
+                return true;
+            } else if (erLedig(medarbejder) == true) {
+                System.out.println("Medarbejder " + medarbejder + " er tilgængelig for projektet " + projekt);
+                return true;
             } else {
                 System.out.println("Ingen projektleder eller ledig medarbejder tilgængelig for projektet " + projekt);
+                return false;
+                }
             }
-        
+            else {
+                System.out.println("Medarbejder " + medarbejder + " findes ikke i systemet.");
+                return false;
+            }
     }
     @When("projektleder eller medarbejder opretter en aktivitet med navn {String}")
-    public Aktivitet opretAktivitet(String aktivitetsnavn) {
-        
-
-        Aktivitet aktivitet = new Aktivitet(aktivitetsnavn);
-        return aktivitet;
+    public void opretAktivitet(String aktivitetsnavn) {
+        this.oprettetAktivitet = new Aktivitet(aktivitetsnavn);
     }
 
     @Then("Oprettelse af aktivitet med navn {String} skal lykkes")
-    public boolean  opretAktivitetSuccess(Object aktivitetsnavn) {
-        
-        if (geteksisterendeAktivitet(aktivitetsnavn) == null) {
+    public boolean opretAktivitetSuccess(String aktivitetsnavn) {
+        if (this.oprettetAktivitet != null && this.oprettetAktivitet.getNavn().equals(aktivitetsnavn)) {
             System.out.println("aktivitet oprettet: " + aktivitetsnavn);
             return true;
-        } else {
-                return false;
         }
+        return false;
     }
 
     @Given ("der findes et projekt med navn {String} i systemet")
     public void getprojekt(String projekt) {
-        // Implementation for setting up the project in the system
         System.out.println("Projekt " + projekt + " findes i systemet.");
     }
     @And ("der findes allerede en aktivitet med navn {String} i projektet")
-    public String geteksisterendeAktivitet(Object activitetsnavn) {
+    public boolean geteksisterendeAktivitet(Object activitetsnavn) {
        
-        if (activitetsnavn.equals("Eksisterende Aktivitet")) {
-            return "Aktivitet findes allerede";
+        if (activitetsnavn in eksisterendeAktiviteter) {
+            return true;
         } else {
-            return null;
+            return false;
         }
     }
     @Then("Oprettelse af aktivitet med navn {String} skal fejle")
     public boolean opretAktivitetFailure(String activitetsnavn) {
-        if(geteksisterendeAktivitet(activitetsnavn) != null) {
+        if(geteksisterendeAktivitet(activitetsnavn) == true) {
             System.out.println("Fejl: Aktivitet med navn '" + activitetsnavn + "' findes allerede i projektet.");
             return true;
         } else {
@@ -76,5 +86,5 @@ public class aktivitetstepdefinitions {
 }
 
 
-*/
+
 
