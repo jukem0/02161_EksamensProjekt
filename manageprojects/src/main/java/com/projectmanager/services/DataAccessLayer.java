d81122eebc51772962523aa7f232c3fe44b4699d
Cobraking364 <gustav.emil.k@gmail.com>
2026-05-09 19:39:43 +0200

    Added DataAccessLayer to read and write to and from the database

---
+++ manageprojects/src/main/java/com/projectmanager/services/DataAccessLayer.java
@@ -0,0 +1,42 @@
+package com.projectmanager.services;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import com.projectmanager.model.Employee;
+
+public class DataAccessLayer {
+
+    public ArrayList<Employee> parseToEmployee(File fileToParse) throws FileNotFoundException{
+        Scanner scanner = new Scanner(fileToParse);
+        ArrayList<Employee> returnVal = new ArrayList<>();
+        while (scanner.hasNextLine()) { 
+            Employee tempEmployee = new Employee(scanner.nextLine());
+            returnVal.add(tempEmployee);
+        }
+        scanner.close();
+        return returnVal;
+    }
+
+    public ArrayList<Employee> parseToEmployee(String fileToParseString) throws FileNotFoundException{
+        File fileToParse = new File(fileToParseString);
+        return parseToEmployee(fileToParse);
+    }
+
+    public void writeToDatabase(String fileName, String input) throws IOException{
+        FileWriter writer = new FileWriter(fileName, true);
+        writer.write(input);
+        writer.close();
+    }
+
+    public void writeToDatabase(String fileName, List<String> inputs) throws IOException{
+        for (String s : inputs) {
+            writeToDatabase(fileName, s);
+        }
+    }
+}
