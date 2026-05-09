4e1935fda02ab6070568f4417e523edc914da60d
SaraNorsJensen <101750184+SaraNorsJensen@users.noreply.github.com>
2026-05-09 14:23:15 +0200

    made FuckCucumber :)

---
+++ manageprojects/src/test/java/stepdefinitions/FuckCucumber.java
@@ -0,0 +1,25 @@
+package stepdefinitions;
+
+import com.projectmanager.model.Activity;
+import com.projectmanager.model.Employee;
+import com.projectmanager.model.Project;
+
+public class FuckCucumber {
+
+    public Project getProject(String project){
+        return null;
+    }
+
+    public Employee getEmployee(String emp){
+        return null;
+    }
+
+    public Activity getActivity(String emp){
+        return null;
+    }
+
+    
+
+
+
+}
