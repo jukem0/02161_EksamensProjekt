47865d0549080e038c7a9e3fd76011760dabe1d4
Alex <alexb@live.dk>
2026-05-10 17:56:29 +0200

    budgettime changes

--- manageprojects/src/test/java/stepdefinitions/CreateProjectSteps.java
+++ manageprojects/src/test/java/stepdefinitions/CreateProjectSteps.java
@@ -1,47 +1,29 @@
 package stepdefinitions;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.ArrayList;
-import java.io.*;
+
+import static org.junit.Assert.assertEquals;
 
 import com.projectmanager.model.Employee;
 import com.projectmanager.model.Project;
-import com.projectmanager.services.DataAccessLayer;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class CreateProjectSteps {
-    private String foundEmployee = "";
-    private ArrayList<Employee> employeesFromFile;
     private ArrayList<Project> projects;
-    public File employeeFile = new File("src/main/java/com/projectmanager/HR/Employees.txt");
-
-    public CreateProjectSteps() {
-        try {
-            DataAccessLayer data = new DataAccessLayer();
-            if (!employeeFile.exists()) {
-                employeeFile = new File("manageprojects/" + employeeFile.getPath());
-            }
-            employeesFromFile = data.parseToEmployee(employeeFile);
-        } catch (FileNotFoundException e) {
-            System.err.println("FEJL: Kunne ikke finde Employees.txt på: " + employeeFile.getAbsolutePath());
-            employeesFromFile = new ArrayList<>();
-        }
-    }
+    public static ArrayList<Employee> employees = new ArrayList<>();
 
     @Given("en {string} findes i systemet")
-    public void employee_findes_i_systemet(String empName) {
-        for (int i = 0; i < employeesFromFile.size(); i++) {
-            if (employeesFromFile.get(i).getEmployeeName().equals(empName)) {
-                foundEmployee = empName;
-                System.out.println(foundEmployee);
-                System.out.println(empName);
+    public void medarbejder_findes_i_systemet(String empName) {
+        for (int i = 0; i < employees.size(); i++) {
+            if (!empName.equals(employees.get(i).getEmployeeName())) {
+                Employee newEmp = new Employee(empName);
+                employees.add(newEmp);
             }
+            assertEquals(empName, employees.get(i).getEmployeeName());
         }
-        assertEquals(empName, foundEmployee);
     }
 
     @When("en medarbejder opretter et projekt med navn {string}")
