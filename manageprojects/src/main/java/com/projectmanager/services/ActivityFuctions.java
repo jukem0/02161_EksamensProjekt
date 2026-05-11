28a16d067707715217029e591ca7ee4b0547390e
Alex <alexb@live.dk>
2026-05-11 11:53:06 +0200

    yay

--- manageprojects/src/main/java/com/projectmanager/services/ActivityFuctions.java
+++ manageprojects/src/main/java/com/projectmanager/services/ActivityFuctions.java
@@ -1,40 +1,52 @@
 package com.projectmanager.services;
 
+import com.projectmanager.model.Activity;
 import com.projectmanager.model.Week;
 
-public class ActivityFuctions implements  IActivityFunctions{
+public class ActivityFuctions implements IActivityFunctions {
 
     @Override
-    public void addActivity(String name, Double budgetTime, Week endDate, int weekAmount) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void setStartDate(Week startDate) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void setEndDate(Week endDate) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void setBudgetTime(double budgetTime) throws IllegalArgumentException {
-
-        if (budgetTime >0){
-
+    public void addActivity(String projectNr, String name, Double budgetTime, Week endDate, int weekAmount) {
+        for (int j = 0; j < RuntimeContext.getProjects().size(); j++) {
+            if (RuntimeContext.getProjects().get(j).getProjectNr().equals(projectNr)) {
+                RuntimeContext.getProjects().get(j).addActivity(name, budgetTime, endDate, weekAmount);
+            }
         }
-        else 
-        {
-            throw new IllegalArgumentException("Budgetteret tidskal væreet positivtdecimaltal eller heltal");
-        }
-        
     }
 
     @Override
-    public void setduration(int weekAmount) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void setEndDate(String projectNr, Activity activityName, Week endDate) {
+        for (int j = 0; j < RuntimeContext.getProjects().size(); j++) {
+            if (RuntimeContext.getProjects().get(j).getProjectNr().equals(projectNr)) {
+                RuntimeContext.getProjects().get(j).getActivityMap().keySet().stream()
+                        .filter(a -> a.equals(activityName)).findFirst().orElse(null).setEndWeek(endDate);
+            }
+        }
+    }
+
+    @Override
+    public void setBudgetTime(String projectNr, Activity activityName, double budgetTime)
+            throws IllegalArgumentException {
+        if (budgetTime < 0) {
+            throw new IllegalArgumentException("Time Budget cannot be negative");
+        }
+        for (int j = 0; j < RuntimeContext.getProjects().size(); j++) {
+            if (RuntimeContext.getProjects().get(j).getProjectNr().equals(projectNr)) {
+                RuntimeContext.getProjects().get(j).getActivityMap().keySet().stream()
+                        .filter(a -> a.equals(activityName)).findFirst().orElse(null).setBudgetTime(budgetTime);
+            }
+        }
+    }
+
+    @Override
+    public void setduration(String projectNr, Activity activityName, int weekAmount) {
+        for (int j = 0; j < RuntimeContext.getProjects().size(); j++) {
+            if (RuntimeContext.getProjects().get(j).getProjectNr().equals(projectNr)) {
+                RuntimeContext.getProjects().get(j).getActivityMap().keySet().stream()
+                        .filter(a -> a.equals(activityName)).findFirst().orElse(null).setWeekAmount(weekAmount);
+
+            }
+        }
     }
 
 }
