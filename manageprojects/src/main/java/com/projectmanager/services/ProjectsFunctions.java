98d2228571601e15602526cf8522cf0e9bb31871
Gui256947 <s256947@dtu.dk>
2026-05-11 11:29:24 +0200

    fjerner concatenation warning

--- manageprojects/src/main/java/com/projectmanager/services/ProjectsFunctions.java
+++ manageprojects/src/main/java/com/projectmanager/services/ProjectsFunctions.java
@@ -2,25 +2,25 @@ package com.projectmanager.services;
 
 import java.util.List;
 
+import com.projectmanager.model.Activity;
 import com.projectmanager.model.Project;
 
-public class ProjectsFunctions implements IProjectFunctions {
 
+public class ProjectsFunctions implements IProjectFunctions {
     @Override
     public String MakeReport(Project project) {
         StringBuilder sb = new StringBuilder();
-        List<Project> projects = runtimeContext.getProjects();
+        List<Project> projects = RuntimeContext.getProjects();
         Project curProject = projects.get(Integer.parseInt(project.getProjectNr()) - 1);
 
-        sb.append("Project report\n");
-        sb.append("Project name: " + curProject.getName() + "\n");
-        sb.append("Project number: " + curProject.getProjectNr() + "\n");
+        sb.append("Project report\n").append("Project name: ").append(curProject.getName()).append("\n").
+        append("Project number: ").append(curProject.getProjectNr()).append("\n");
 
         //add activities
         sb.append("\nActivities:");
         for(Activity a : curProject.getActivityMap().keySet()) {
-            sb.append("\n - " + a.getActivityName() + ":\n");
-            sb.append(" -  Time Budget: " + a.getBudgetTime() + "  -  Time Spend: " + a.getTimeSpend() + "  -  Time Remaining: " + a.getRemainingTime() + "\n");
+            sb.append("\n - ").append(a.getActivityName()).append(":\n");
+            sb.append(" -  Time Budget: ").append(a.getBudgetTime()).append("  -  Time Spend: ").append(a.getTimeSpend()).append("  -  Time Remaining: ").append(a.getRemainingTime()).append("\n");
         }
         
         return sb.toString();
